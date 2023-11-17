285c9356a211a099e2669fa05101e173e47182b4
angelussantaines <150672051+angelussantaines@users.noreply.github.com>
2023-11-17 02:07:00 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,30 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('mi-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                // Aquí puedes agregar otros recursos que deseas cachear
-            ]);
-        })
-    );
+const CACHE_NAME = 'v1_cache_retiro_emaus';
+const urlsToCache = [
+  './',
+  '192x192.png',
+  '512x512.png',
+  'estilo.css', // Si tienes archivos CSS externos
+  'script.js',  // Si tienes archivos JS externos
+  // Agrega aquí todos los otros recursos que necesitas almacenar en caché
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(e.request);
+      })
+  );
 });
